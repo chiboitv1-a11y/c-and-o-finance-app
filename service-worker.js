c310fe53b3d054dc382276d14588e414599cb388
chiboitv1-a11y <chiboitv1@gmail.com>
2026-02-01 00:36:31 +0100

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME = 'co-finance-cache-v1';
-const urlsToCache = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
-self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))); });
-self.addEventListener('fetch', event => { event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request))); });
-self.addEventListener('activate', event => { const cacheWhitelist=[CACHE_NAME]; event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>{if(!cacheWhitelist.includes(k)) return caches.delete(k);})))); });
